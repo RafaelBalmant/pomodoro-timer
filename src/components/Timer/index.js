78180e44ccda61e9b913158be9ca5fb6144e2521
LucaPietro <lucabaronpietro@gmail.com>
2020-09-01 17:06:55 -0300

    Added input mask

--- src/components/Timer/index.js
+++ src/components/Timer/index.js
@@ -1,26 +1,59 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Button from "@material-ui/core/Button";
+import InputBase from "@material-ui/core/InputBase";
+import MaskedInput from "react-text-mask";
 
 import "./styles.css";
 
+function TextMaskCustom(props) {
+  const { inputRef, ...other } = props;
+
+  return (
+    <MaskedInput
+      {...other}
+      ref={(ref) => {
+        inputRef(ref ? ref.inputElement : null);
+      }}
+      mask={[/[0-5]/, /[0-9]/, ":", /[0-5]/, /[0-9]/]}
+      placeholderChar={"\u2000"}
+      showMask
+    />
+  );
+}
+
 const Timer = () => {
-  const [counter, setCounter] = useState(4);
+  const [minutes, setMinutes] = useState("00");
+  const [seconds, setSeconds] = useState("04");
   const [hasStarted, setHasStarted] = useState(false);
 
-  React.useEffect(() => {
-    if (counter <= 0) {
+  function handleTime(e) {
+    //console.log(e);
+    const [min, sec] = e.target.value.split(":");
+    //console.log(min, sec);
+    setMinutes(min);
+    setSeconds(sec);
+  }
+
+  useEffect(() => {
+    if (minutes === "00" && seconds === "00") {
       setHasStarted(false);
     } else {
       hasStarted &&
-        counter > 0 &&
         setTimeout(() => {
-          setCounter((counter - 0.01).toFixed(2));
-        }, 10);
-      if (counter === 0) {
-        setHasStarted(!hasStarted);
-      }
+          if (seconds > 0 && seconds < 10) {
+            setSeconds("0" + (parseInt(seconds) - 1).toString());
+          } else if (seconds > 0) {
+            setSeconds((parseInt(seconds) - 1).toString());
+          } else if (minutes > 0 && minutes < 10) {
+            setSeconds("59");
+            setMinutes("0" + (parseInt(minutes) - 1).toString());
+          } else {
+            setSeconds("59");
+            setMinutes((parseInt(minutes) - 1).toString());
+          }
+        }, 1000);
     }
-  }, [counter, hasStarted]);
+  }, [hasStarted, minutes, seconds]);
 
   function toggleHasStarted() {
     setHasStarted(!hasStarted);
@@ -28,10 +61,11 @@ const Timer = () => {
 
   return (
     <>
-      <input
-        value={Math.ceil(counter)}
-        onChange={(e) => setCounter(e.target.value)}
-      ></input>
+      <InputBase
+        value={minutes + seconds}
+        onChange={handleTime}
+        inputComponent={TextMaskCustom}
+      />
 
       <Button
         variant="contained"
